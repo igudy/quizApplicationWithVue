a2aa0bf302518ad7c500d38fba0c017904f8821e
Goodness Igunma Igudy <Goodnessigunma1@gmail.com>
2021-09-06 15:08:48 +0100

    Added questions

---
+++ src/components/Questions.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="questions-ctr">
+        <div class="progress">
+            <div class="bar"></div>
+            <div class="status">
+                1 out of 3 questions answered
+            </div>
+        </div>
+        <div class="single-question"
+             v-for="question in questions"
+             :key="question.q"
+         >
+            <div class="question">{{question.q}}</div>
+            <div class="answers" 
+                 v-for="answer in questions.answers"
+                 :key="answer.text"
+            >
+                <div class="answer">{{ answer.text }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['questions'],
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,18 @@
+<template>
+      <div class="result">
+        <div class="title">You got sv ample result 1!</div>
+        <div class="desc">
+            Enter a short description here about the result.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
