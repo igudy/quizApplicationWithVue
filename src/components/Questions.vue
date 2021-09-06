3bcfa0156ff5d6b706ce9f6ce6974b953b4a4d51
Goodness Igunma <gooodnessigunma1@gmail.com>
2021-09-07 00:28:03 +0100

    Done with the quiz app and added animation

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,32 +1,45 @@
 <template>
-    <div class="questions-ctr">
-        <div class="progress">
-            <div class="bar"></div>
-            <div class="status">
-                1 out of 3 questions answered
-            </div>
-        </div>
-        <div class="single-question"
-             v-for="question in questions"
-             :key="question.q"
-         >
-            <div class="question">{{question.q}}</div>
-            <div class="answers" 
-                 v-for="answer in questions.answers"
-                 :key="answer.text"
-            >
-                <div class="answer">{{ answer.text }}</div>
-            </div>
-        </div>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"
+      ></div>
+      <div class="status">
+        {{ questionsAnswered }} out of {{ questions.length }} questions answered
+      </div>
     </div>
+    <transition-group name="fade">
+      <div
+        class="single-question"
+        v-for="(question, qi) in questions"
+        :key="question.q"
+        v-show="questionsAnswered === qi"
+      >
+        <div class="question">{{ question.q }}</div>
+        <div class="answers">
+          <div
+            class="answer"
+            v-for="answer in question.answers"
+            :key="answer.text"
+            @click.prevent="selectAnswer(answer.is_correct)"
+          >
+            {{ answer.text }}
+          </div>
+        </div>
+      </div>
+    </transition-group>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['questions'],
-}
+  props: ["questions", "questionsAnswered"],
+  emits: ["question-answered"],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
